8891d04d2b3cfa3724127ce6c7d87c02fd314b84
Stefan Ziegler <edi.gonzales@gmail.com>
2026-01-23 18:57:11 +0000

    Select XTF reader by INTERLIS version

--- src/main/java/com/interlis/transform/interlis/InterlisIoFactory.java
+++ src/main/java/com/interlis/transform/interlis/InterlisIoFactory.java
@@ -1,8 +1,11 @@
 package com.interlis.transform.interlis;
 
+import ch.interlis.ili2c.metamodel.Model;
 import ch.interlis.ili2c.metamodel.TransferDescription;
 import ch.interlis.iom_j.itf.ItfReader;
 import ch.interlis.iom_j.itf.ItfWriter;
+import ch.interlis.iom_j.xtf.Xtf23Reader;
+import ch.interlis.iom_j.xtf.Xtf24Reader;
 import ch.interlis.iom_j.xtf.XtfReader;
 import ch.interlis.iom_j.xtf.XtfWriter;
 import ch.interlis.iox.IoxReader;
@@ -31,7 +34,7 @@ public final class InterlisIoFactory {
         if (format == InterlisFormat.ITF) {
             reader = new ItfReader(path.toFile());
         } else {
-            reader = new XtfReader(path.toFile());
+            reader = createXtfReader(path, transferDescription);
         }
         if (reader instanceof IoxIliReader iliReader) {
             iliReader.setModel(transferDescription);
@@ -46,5 +49,40 @@ public final class InterlisIoFactory {
             return new ItfWriter(path.toFile(), transferDescription);
         }
         return new XtfWriter(path.toFile(), transferDescription);
+    }
+
+    private IoxReader createXtfReader(Path path, TransferDescription transferDescription) throws Exception {
+        String iliVersion = detectIliVersion(transferDescription);
+        if (Model.ILI2_4.equals(iliVersion)) {
+            return new Xtf24Reader(path.toFile());
+        }
+        if (Model.ILI2_3.equals(iliVersion)) {
+            return new Xtf23Reader(path.toFile());
+        }
+        return new XtfReader(path.toFile());
+    }
+
+    private String detectIliVersion(TransferDescription transferDescription) {
+        Model[] models = transferDescription.getModelsFromLastFile();
+        if (models != null && models.length > 0) {
+            boolean saw23 = false;
+            for (Model model : models) {
+                String version = model.getIliVersion();
+                if (Model.ILI2_4.equals(version)) {
+                    return Model.ILI2_4;
+                }
+                if (Model.ILI2_3.equals(version)) {
+                    saw23 = true;
+                }
+            }
+            if (saw23) {
+                return Model.ILI2_3;
+            }
+        }
+        Model lastModel = transferDescription.getLastModel();
+        if (lastModel != null) {
+            return lastModel.getIliVersion();
+        }
+        return null;
     }
 }
